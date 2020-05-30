767b74b96635f30a127ac560aad56b6fbcd2824c
lukasrafael1407 <lucasrafaelbarbosa@gmail.com>
2020-05-30 10:11:45 -0300

    Atualizando

--- src/containers/home/index.tsx
+++ src/containers/home/index.tsx
@@ -1,9 +1,9 @@
+import { Avatar, Card, Divider, Layout, Text } from '@ui-kitten/components';
+import { Image, ScrollView, View } from 'react-native';
 import React, { Component, } from 'react';
 import { inject, observer } from 'mobx-react';
 
 import HomeStore from '../../stores/home.store';
-import { Layout } from '@ui-kitten/components';
-import { ScrollView } from 'react-native';
 
 interface Props {
   homeStore: HomeStore,
@@ -13,10 +13,33 @@ interface Props {
 @inject('homeStore')
 @observer
 export default class Home extends Component<Props> {
+
+  async componentDidMount() {
+    const { getPosts } = this.props.homeStore;
+    await getPosts();
+  }  
+
   render() {
+
+    const { posts } = this.props.homeStore; 
+    
     return (
       <Layout style={{ flex: 1, backgroundColor: 'black' }}>
         <ScrollView>
+            {posts.map((post, index) => {
+                <Card key={index}>
+                  <View>
+                      <Avatar
+                          size={'small'}
+                          source={{ uri: post.author.avatar }}/>
+                      <Text>{post.author.name}</Text>
+                  </View>                  
+                  <Image source={{ uri: post.image }} />
+                  <Divider />
+                  <View>
+                    <Text>{post.description}</Text>
+                  </View>
+            </Card>})}
         </ScrollView>
       </Layout>);
   }
